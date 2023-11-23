a5092857b9e2245bac01645e0db202beb466a968
Ehnwall <dennis.ehnwall@gmail.com>
2023-11-23 20:40:10 +0100

    small changes  to getting players name.

--- pvp.js
+++ pvp.js
@@ -11,12 +11,18 @@ const players = [
     },
 ];
 
+// Starts the game with the names that the user inputs(if no names, use placeholders)
 startButton.addEventListener("click", (e) => {
     const playerOneName = document.querySelector("#player-one");
     const playerTwoName = document.querySelector("#player-two");
     players[0].name = playerOneName.value;
     players[1].name = playerTwoName.value;
-
+    if (playerOneName.value == "") {
+        players[0].name = playerOneName.getAttribute("placeholder");
+    }
+    if (playerTwoName.value == "") {
+        players[1].name = playerTwoName.getAttribute("placeholder");
+    }
     sessionStorage.setItem("players", JSON.stringify(players));
     // e.preventDefault();
 });
